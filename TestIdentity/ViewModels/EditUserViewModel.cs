c71b5151399499eaf56f3adb444f20513534df05
tanyalev06 <grib.tanya06@mail.ru>
2022-01-15 16:43:25 +0300

    create task without admin panel

---
+++ TestIdentity/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestIdentity.ViewModels
+{
+    public class EditUserViewModel : Controller
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+    }
+}
